b2f2b599efeeb865796cbedbf97412ed0ebb2f07
Luke Bainbridge <luke.bainbridge@gmail.com>
2018-04-11 09:15:59 -0500

    Some additional calendar things.

--- app/Resources/assets/js/main.js
+++ app/Resources/assets/js/main.js
@@ -1,6 +1,1 @@
-window.$ = $ = require('jquery');
 require('bootstrap');
-require('moment');
-window.$.fullCalendar = $.fullCalendar = require('fullcalendar');
-
-$('#calendar').fullCalendar({});
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,33 @@
-// webpack.config.js
-var Encore = require('@symfony/webpack-encore');
+// Plugins and the like.
 var webpack = require('webpack');
+var Encore = require('@symfony/webpack-encore');
 var LiveReloadPlugin = require('webpack-livereload-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 Encore
+    // Create output paths and main entry points.
     .setOutputPath('web/build/')
     .setPublicPath('/build')
-    .cleanupOutputBeforeBuild()
     .addEntry('main', './app/Resources/assets/js/main.js')
     .addStyleEntry('global', './app/Resources/assets/css/global.scss')
-    .addPlugin(new webpack.ProvidePlugin({ Popper: ['popper.js', 'default'] }))
-    .enableSassLoader()
+
+    // Schedule configuration.
+    .addEntry('calendar', './app/Resources/assets/js/calendar.js')
+    .addPlugin(new CopyWebpackPlugin([
+        { from: './node_modules/bootstrap-calendar', to: 'components/bootstrap-calendar' },
+    ]))
+
+    // Add plugins.
     .autoProvidejQuery()
+    .addPlugin(new webpack.ProvidePlugin({ _: 'underscore' }))
+    .addPlugin(new webpack.ProvidePlugin({ Popper: ['popper.js', 'default'] }))
+    .addPlugin(new LiveReloadPlugin())
+
+    // Settings for output.
+    .cleanupOutputBeforeBuild()
+    .enableSassLoader()
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning()
 ;
 
-// export the final configuration
-config = Encore.getWebpackConfig();
-config.plugins.push(new LiveReloadPlugin());
-module.exports = config;
+module.exports = Encore.getWebpackConfig();
